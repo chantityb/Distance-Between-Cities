a300aaf62ed6379f09bfc13a4e5d858e526dbc63
chantityb <chantityb@gmail.com>
2018-06-17 20:39:41 -0400

    created autocomplete objects

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 //set map options
-var myLatLng = {lat: , lng: };
-var mapOptions = {
-	center: myLatLng,
-	zoom: 7,
-	mapTypeId: google.maps.mapTypeId.ROADMAP
-};
+	var myLatLng = {lat: 38.25, lng: -85.75};
+	var mapOptions = {
+		center: myLatLng,
+		zoom: 7,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
 
-//create map
-var map = new
-google.maps.Map(document.getElemntById('googleMap'), mapOptions);
 
-//create a DirectionsService object to use the route method and get a result for our request
-var directionsService = new
-google.maps.DirectionsService();
+	//create map
+	var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 
-//create a DirectionsService object which we will use to display the route
-var directionsDisplay = new
-google.maps.DirectionsRenderer();
+	//create a DirectionsService object to use the route method and get a result for our request
+	var directionsService = new google.maps.DirectionsService();
 
-	//bind the DirectionREnderer to the map
-	directionsDisplay.setMap(map);
+	//create a DirectionsService object which we will use to display the route
+	var directionsDisplay = new google.maps.DirectionsRenderer();
+
+		//bind the DirectionREnderer to the map
+		directionsDisplay.setMap(map);
+
 
 //define calcRoute function
 function calcRoute(){
 	//create request
 	var request = {
-		origin: document.getElemntById("from").value,
-		destinations: document.getElemntById("to").value,
+		origin: document.getElementById("from").value,
+		destination: document.getElementById("to").value,
 		travelMode: google.maps.TravelMode.DRIVING,
 	//WALKING, BYCYCLING, TRANSIT
-		unitSystem: google.map.UnitSystem.IMPERIAL	
+		unitSystem: google.maps.UnitSystem.IMPERIAL	
 	}
 
 	//pass the request to the route method
@@ -38,7 +37,7 @@ function calcRoute(){
 		if(status == google.maps.DirectionsStatus.OK){
 
 			//Get distance and time
-			$("#output").html("<div class='alert-info'>From: " + document.getElemntById("from").value + ".<br />To: " + document.getElemntById("to").value + ".<br /> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />Duraimoh: " + result.routes[0].legs[0].duration.text + ".</div>");
+			$("#output").html("<div class='alert-info'>From:" +document.getElementById("from").value+ ".<br />To:" +document.getElementById("to").value+ ".<br /> Driving distance: " +result.routes[0].legs[0].distance.text+ ".<br/>Duration:" +result.routes[0].legs[0].duration.text+ ".</div>");
 			
 			//display route
 			directionsDisplay.setDirections(result);
@@ -53,15 +52,18 @@ function calcRoute(){
 	});
 }
 
+
 //create autocomplete objects for all inputs
 var options = {
 	types: ['(cities)']
 }
 
-var input1 = docutment.getElemntById("from");
+var input1 = document.getElementById("from");
 var autocomplete1 = new
 google.maps.places.Autocomplete(input1, options);
 
-var input 2 = document.getElemntById("to");
+var input2 = document.getElementById("to");
 var autocomplete2 = new
 google.maps.places.Autocomplete(input2, options);
+
+
